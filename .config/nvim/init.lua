71e97980fbe9668baf5ae8f1161a7cc5679cbb45
BbqGamer <korba.adam@gmail.com>
2024-08-16 14:42:12 +0200

    Add lazygit wrapper to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -67,6 +67,26 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	"tpope/vim-fugitive",
 	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			{ "<leader>lc", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "CurrentFile" },
+		},
+	},
+	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
