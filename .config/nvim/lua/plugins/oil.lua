8e922363dbb409cc933412fdd7c4279189ae545d
BbqGamer <korba.adam@gmail.com>
2024-10-06 00:41:11 +0200

    Update oil.nvim configuration

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,5 +1,8 @@
 return {
 	"stevearc/oil.nvim",
+	lazy = false,
+	---@module 'oil'
+	---@type oil.SetupOpts
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
 		{
@@ -11,5 +14,13 @@ return {
 	},
 	opts = {
 		default_file_explorer = true,
+		columns = {
+			"icon",
+			"permissions",
+			"size",
+			"mtime",
+		},
+		delete_to_trash = false,
+		skip_confirm_for_simple_edits = true,
 	},
 }
