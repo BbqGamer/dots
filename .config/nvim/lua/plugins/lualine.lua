72a23a9b87fa71946a3d962f7af4bca055a86d00
BbqGamer <korba.adam@gmail.com>
2024-10-18 14:08:00 +0200

    nvim(lualine): relative fnames and del fileformat

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,5 +5,24 @@ return {
 		options = {
 			theme = "nord",
 		},
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					path = 1,
+				},
+			},
+			lualine_x = { "encoding", "filetype" },
+		},
+		inactive_sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true,
+					path = 1,
+				},
+			},
+		},
 	},
 }
