6913c3def59865c1b0faeaf51c099266814f254b
BbqGamer <korba.adam@gmail.com>
2026-04-26 23:12:19 +0200

    feat(nvim): add telescope fzf native matcher

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -36,6 +36,7 @@ return {
 		{ "nvim-tree/nvim-web-devicons", enabled = true },
 		"nvim-telescope/telescope-ui-select.nvim",
 		{ "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	opts = function()
 		local actions = require("telescope.actions")
@@ -53,6 +54,12 @@ return {
 				},
 			},
 			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
 				["ui-select"] = themes.get_dropdown({}),
 				live_grep_args = {
 					auto_quoting = false,
@@ -72,6 +79,7 @@ return {
 		local telescope = require("telescope")
 
 		telescope.setup(opts)
+		telescope.load_extension("fzf")
 		telescope.load_extension("ui-select")
 		telescope.load_extension("live_grep_args")
 	end,
@@ -108,7 +116,7 @@ return {
 		{
 			"<leader>pf",
 			function()
-				require("telescope.builtin").fd({ hidden = true })
+				require("telescope.builtin").find_files({ hidden = true })
 			end,
 			desc = "Find files",
 		},
